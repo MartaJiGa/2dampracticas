55a630084e4339c45d9d272a5fc662dae3627634
Dviela <a19230@svalero.com>
2025-01-31 17:44:54 +0100

    Add javafx structure and main

---
+++ JavaFX/src/Main.java
@@ -0,0 +1,21 @@
+package javafxbooks;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/books-view.fxml"));
+        Scene scene = new Scene(loader.load(), 700, 500);
+        primaryStage.setTitle("Lista de Libros");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
