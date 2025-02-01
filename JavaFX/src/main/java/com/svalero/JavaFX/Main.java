adf5772d70c7c812730ce61a6a33ae077de8d0d3
Dviela <a19230@svalero.com>
2025-02-01 16:32:21 +0100

    JavaFx final version

--- JavaFX/src/main/java/com/svalero/JavaFX/Main.java
+++ JavaFX/src/main/java/com/svalero/JavaFX/Main.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/svalero/JavaFX/view.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/svalero/JavaFX/main_view.fxml"));
         Scene scene = new Scene(loader.load(), 700, 500);
         primaryStage.setTitle("Lista de Libros");
         primaryStage.setScene(scene);
